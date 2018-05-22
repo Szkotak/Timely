bc8dd715a9f50d365e46b3784ef3b270951ce8d8
skanakia <skanakia1@gmail.com>
2018-05-22 13:30:43 -0400

    submit button functionality

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,19 @@
-$('#textarea1').val('New Text');
-  M.textareaAutoResize($('#textarea1'));
+// $('#textarea1').val('New Text');
+//   M.textareaAutoResize($('#textarea1'));
+var labels = [];
+var data = [];
 
+$( document ).ready(function() {
+    $(".button1").on("click", function(){
+        if ($("#act1").val() !== "" && $("#act2").val() !== "" && $("#act3").val() !== "" && $("#act4").val() !== "" && $("#act5").val() !== "") {
+            for (var i = 1; i < 6; i++) {
+                var activity = $("#act" + i).val();
+                data.push(activity);
+            }
+            data.push("Other");
+            console.log(data);
+        } else {
+            console.log("empty");
+        }
+    });
+});
