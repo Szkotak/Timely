682cda437c8715033cd3cb758b0f535800e30a38
mecaputy <martina.e.caputy@gmail.com>
2018-05-22 18:56:51 -0400

    sliders

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,12 +8,35 @@ $( document ).ready(function() {
         if ($("#act1").val() !== "" && $("#act2").val() !== "" && $("#act3").val() !== "" && $("#act4").val() !== "" && $("#act5").val() !== "") {
             for (var i = 1; i < 6; i++) {
                 var activity = $("#act" + i).val();
-                data.push(activity);
+                labels.push(activity);
             }
-            data.push("Other");
-            console.log(data);
+            labels.push("Other");
+            console.log(labels);
         } else {
             console.log("empty");
         }
     });
+    
 });
+
+// var slider = document.getElementById('slider');
+// noUiSlider.create(slider, {
+//  start: [20, 80],
+//  connect: true,
+//  step: 1,
+//  orientation: 'horizontal', // 'horizontal' or 'vertical'
+//  range: {
+//    'min': 0,
+//    'max': 100
+//  },
+//  format: wNumb({
+//    decimals: 0
+//  })
+// });
+//create a function tha clears the form and replaces it with sliders using the new user input as the activities
+
+// once the user inputs variables with the data of current activity save in local
+//display that data in bottom left box
+//move to next window to set goals
+// once goals are set input data into box
+//create donut chart for the variables for the current and goal time
